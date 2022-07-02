982b0405faefb0609814cbf44817922320703de1
Joe Wilson <wilsonmangunsong@gmail.com>
2022-07-02 09:02:06 +0700

    sec9_vid49_to_vid50
    Develop JPA Entities

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/wilson/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,26 @@
+package com.wilson.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+// @Data -- known bug, if you make relationship between data like OneToMany and ManyToOne
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category") // mapped by variable category in Product entity
+    private Set<Product> products;
+}
