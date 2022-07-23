c27143eb3f617707cd0ff6ad848493651957e613
Joe Wilson <wilsonmangunsong@gmail.com>
2022-07-23 14:52:01 +0700

    sec13_vid88
    Display category name in product list grid

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ProductCategoryMenuComponent } from './components/product-category-menu
 
 const routes: Routes = [
   // kindly reminder, the order of routes is IMPORTANT
-  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category/:id/:name', component: ProductListComponent},
   {path: 'category', component: ProductListComponent},
   {path: 'products', component: ProductListComponent},
   {path: '', redirectTo: '/products', pathMatch:'full'},
--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products?: Product[];
   currentCategoryId?: number;
+  currentCategoryName?: string;
   // inject our ProductService
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -31,10 +32,14 @@ export class ProductListComponent implements OnInit {
       // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.params["id"]; // +this.route.snapshot.paramMap.get('id');
       // + parameter value is returned as string. Use the "+" symbol to convert to number
+
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name') || "";
     }
     else {
-      // not category_id available... default to cateogry id 1
+      // not category_id available... default to cateogry id 1 and category name book
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
     // now get the products for the given category id
