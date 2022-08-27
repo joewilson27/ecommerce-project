e4a37a19abeef84304569de370e7b9e30d97da94
Joe Wilson <wilsonmangunsong@gmail.com>
2022-08-27 13:19:23 +0700

    section20_vid167
    Add interfaces for State Repository

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/wilson/ecommerce/dao/StateRepository.java
@@ -0,0 +1,20 @@
+package com.wilson.ecommerce.dao;
+
+import com.wilson.ecommerce.entity.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
+public interface StateRepository extends JpaRepository<State, Integer> {
+
+    // query for finding states by the country code
+    List<State> findByCountryCode(@Param("code") String code);
+    // this allow us to get the States by the country code in the API like this:
+    // http://localhost:8080/api/states/search/findByCountryCode?code=US
+
+}
