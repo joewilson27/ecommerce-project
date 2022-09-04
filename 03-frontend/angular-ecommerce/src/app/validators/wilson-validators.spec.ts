ddc077298fb7e2eeaf2cf298ac5ed780e41c31bb
Joe Wilson <wilsonmangunsong@gmail.com>
2022-09-04 16:59:14 +0700

    section21_vid182_to_vid193
    Section 21 vid 183 - Custom Validation Rule

---
+++ 03-frontend/angular-ecommerce/src/app/validators/wilson-validators.spec.ts
@@ -0,0 +1,7 @@
+import { WilsonValidators } from './wilson-validators';
+
+describe('WilsonValidators', () => {
+  it('should create an instance', () => {
+    expect(new WilsonValidators()).toBeTruthy();
+  });
+});
