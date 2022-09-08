fd952cf779938f95afdc9545a323fc634ad362eb
Joe Wilson <wilsonmangunsong@gmail.com>
2022-09-08 22:35:34 +0700

    section23_vid198_to_vid202
    Section 23 vid 199
    Download save order database
    www.luv2code.com/save-the-order-starter-files.zip

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/wilson/ecommerce/entity/Order.java
@@ -0,0 +1,61 @@
+package com.wilson.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="order_tracking_number")
+    private String orderTrackingNumber;
+
+    @Column(name="total_quantity")
+    private int totalQuantity;
+
+    @Column(name="total_price")
+    private BigDecimal totalPrice;
+
+    @Column(name="status")
+    private String status;
+
+    @Column(name="date_created")
+    @CreationTimestamp // automatically create timestamp
+    private Date dateCreated;
+
+    @Column(name="last_updated")
+    @UpdateTimestamp // automatically update timestamp
+    private Date lastUpdated;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "order") // mapped by order variable in OrderItem entity
+    private Set<OrderItem> orderItems = new HashSet<>();
+
+    // convenience method
+    public void add(OrderItem item) {
+
+        if (item != null) {
+            if (orderItems == null) {
+                orderItems = new HashSet<>();
+            }
+
+            orderItems.add(item);
+            item.setOrder(this); // bi-directional relationship set up
+        }
+
+    }
+}
