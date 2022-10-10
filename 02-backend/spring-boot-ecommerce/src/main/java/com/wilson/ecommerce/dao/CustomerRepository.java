bbf50e45971d77bf9dcbed1975e7e71c70631d54
Joe Wilson <wilsonmangunsong@gmail.com>
2022-10-10 22:31:53 +0700

    section28_vid244_to_vid245
    handle customer by email - coding backend

--- 02-backend/spring-boot-ecommerce/src/main/java/com/wilson/ecommerce/dao/CustomerRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/wilson/ecommerce/dao/CustomerRepository.java
@@ -4,4 +4,6 @@ import com.wilson.ecommerce.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findByEmail(String theEmail); // same as -> SELECT * FROM customer c WHERE c.email = theEmail // return null if not found
 }
