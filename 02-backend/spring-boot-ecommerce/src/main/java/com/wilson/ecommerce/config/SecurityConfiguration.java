4790c5f416dbc35b619a7127d88ebce2d05ffc80
Joe Wilson <wilsonmangunsong@gmail.com>
2022-11-01 22:55:00 +0700

    sec30_vid267
    Section 30 vid 267
    coba jalankan api http://localhost:8080/api/orders/search/findByCustomerEmail?email=ecommercejoe27@proton.me di postman dan browser, maka akan mendapatkan error 401
    
    when we run the application and login, try to open the orders page, then you can find 401 unauthorized. Why? padahal kita login? ya kita hanya login di bagian frontend, namun pada saat itu, request dr frontend ke backend kita butuh meng-update access token dengan HTTP request agar tidak 401

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/wilson/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.wilson.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // protect endpoint /api/orders
+        // Lambda expression using Spring Security Lambda DSL
+        http.authorizeRequests(configurer ->
+                        configurer
+                                .antMatchers("/api/orders/**") // protect this endpoint
+                                .authenticated())
+                .oauth2ResourceServer() // configures OAuth2 Resource Server support
+                .jwt(); // Enables JWT-encoded bearer token support
+
+        // add CORS filter
+        http.cors();
+
+        // add content negotiation strategy, to support Okta sending back a friendly response
+        http.setSharedObject(ContentNegotiationStrategy.class,
+                            new HeaderContentNegotiationStrategy());
+
+        // force a non-empty response body for 401's to make the response more friendly
+        Okta.configureResourceServer401ResponseBody(http);
+
+        return http.build(); // HttpSecurity supports the Builder design pattern hence, we can "build it" to return the instance
+    }
+
+}
