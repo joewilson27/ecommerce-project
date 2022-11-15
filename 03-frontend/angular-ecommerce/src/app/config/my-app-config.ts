9e6b0ac93ec38a0f3142f0528637fed21f9954eb
Joe Wilson <wilsonmangunsong@gmail.com>
2022-11-15 22:56:35 +0700

    Update app and Okta to https

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa6mp61dlwaxJYYX5d7', // from okta, public id
         issuer: 'https://dev-00085090.okta.com/oauth2/default', // URL when authorizing with Okta Authorization Server
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
         /**
          * scopes information:
