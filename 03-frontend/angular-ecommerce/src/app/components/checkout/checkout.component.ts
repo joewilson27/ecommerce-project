ded3ae92003802857260afc15af7ab6cecc48ab3
Joe Wilson <wilsonmangunsong@gmail.com>
2022-12-06 23:10:12 +0700

    sec35_vid307

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -135,7 +135,30 @@ export class CheckoutComponent implements OnInit {
   }
   
   setupStripePaymentForm() {
-    throw new Error('Method not implemented.');
+    
+    // get a handle to stripe elements
+    var elements = this.stripe.elements();
+
+    // create a card element ... and hide the zip-code field
+    this.cardElement = elements.create('card', { hidePostalCode: true });
+
+    // Add an instance of card UI component into the 'card-element' div
+    this.cardElement.mount('#card-element'); // ini pada div id card-element di checkout.component.html utk di inject
+
+    // Add event binding for the 'change' event on the card element
+    this.cardElement.on('change', (event: any) => {
+
+      // get a handle to card-errors element
+      this.displayError = document.getElementById('card-errors');
+
+      if (event.complete) {
+        this.displayError.textContent = "";
+      } else if (event.error) {
+        // show validation error to customer
+        this.displayError.textContent = event.error.message;
+      }
+    });
+
   }
 
   reviewCartDetails() {
